3ec8bf7bbe1ef4d6018fc3191b538eaf35d4b795
Vatsal Parekh <contact@vatsalparekh.com>
2019-07-28 14:16:36 -0400

    ran file thorugh black

--- local_md.py
+++ local_md.py
@@ -12,16 +12,17 @@ DIRECTORY = None
 
 app = Bottle()
 
+
 def is_md(file):
     """Simple check for filtering
     """
-    if ".md" in file or \
-     ".rst" in file:
+    if ".md" in file or ".rst" in file:
         return True
     else:
         return False
 
-@app.route('/')
+
+@app.route("/")
 def index():
     """Index route
 
@@ -30,16 +31,20 @@ def index():
     files = []
     for (_, dir_names, file_names) in walk(DIRECTORY):
         files.extend(list(map(lambda x: (DIRECTORY.joinpath(x), x), dir_names)))
-        files.extend(list(map(lambda x : (DIRECTORY.joinpath(x), x), filter(is_md,file_names))))
+        files.extend(
+            list(map(lambda x: (DIRECTORY.joinpath(x), x), filter(is_md, file_names)))
+        )
         break
     return template("index.html", files=files)
 
-@app.route('/static/<file_path:path>')
+
+@app.route("/static/<file_path:path>")
 def server_static(file_path):
     """Static files(css)
     """
     path = PurePath(__file__).parent.joinpath("static")
     return static_file(file_path, root=path)
+
 
 @app.route("/<path:path>")
 def serve_md(path):
@@ -52,28 +57,60 @@ def serve_md(path):
         files.append((PurePath(path).parent, ".."))
         path = PurePath(path)
         for (_, dir_names, file_names) in walk(path):
-            files.extend(list(map(lambda x: (path.joinpath(PurePath(x).name), PurePath(x).name), dir_names)))
-            files.extend(list(map(lambda x: (path.joinpath(x), PurePath(x).name), filter(is_md,file_names))))
+            files.extend(
+                list(
+                    map(
+                        lambda x: (path.joinpath(PurePath(x).name), PurePath(x).name),
+                        dir_names,
+                    )
+                )
+            )
+            files.extend(
+                list(
+                    map(
+                        lambda x: (path.joinpath(x), PurePath(x).name),
+                        filter(is_md, file_names),
+                    )
+                )
+            )
             break
         return template("index.html", files=files)
     if is_md(path):
         if ".md" in path:
             with open(path) as md:
-                html = markdown.markdown(md.read(), extensions=['codehilite', 'fenced_code'])
+                html = markdown.markdown(
+                    md.read(), extensions=["codehilite", "fenced_code"]
+                )
         else:
             with open(path) as rst:
-                html = publish_string(rst.read(), writer_name='html').decode()
+                html = publish_string(rst.read(), writer_name="html").decode()
         return template("markdown.html", html=html, path=path)
     else:
         return template("nope.html")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Serve markdown/rst files in a browser as html")
-    parser.add_argument("--directory", help="Serve md files from this directory, if not passed uses current directory")
-    parser.add_argument('--host', default="localhost", help="Host ip/name. Default localhost.")
-    parser.add_argument('--port', default=8080, type=int, help="Port number on which to serve. Default 8080.")
-    parser.add_argument('--server', default="wsgiref", help="Serve to use like paste, etc. Default wsgiref.")
+    parser = argparse.ArgumentParser(
+        description="Serve markdown/rst files in a browser as html"
+    )
+    parser.add_argument(
+        "--directory",
+        help="Serve md files from this directory, if not passed uses current directory",
+    )
+    parser.add_argument(
+        "--host", default="localhost", help="Host ip/name. Default localhost."
+    )
+    parser.add_argument(
+        "--port",
+        default=8080,
+        type=int,
+        help="Port number on which to serve. Default 8080.",
+    )
+    parser.add_argument(
+        "--server",
+        default="wsgiref",
+        help="Serve to use like paste, etc. Default wsgiref.",
+    )
     args = parser.parse_args()
     DIRECTORY = PurePath(args.directory) if args.directory else None
     app.run(server=args.server, host=args.host, port=args.port)
